528cdae9cd618a80a592f7098c35d21f0b4ca5b0
RileyT420 <90624488+RileyT420@users.noreply.github.com>
2023-06-05 14:59:04 +0000

    .

--- script.js
+++ script.js
@@ -1,36 +1,39 @@
-let worldChart = document.getElementById('worldChart').getContext('2d');
+let chart = document.getElementById('worldChart').getContext('2d');
 Chart.defaults.color = 'black';
-var bulking = document.getElementById('bulking')
-var cutting = document.getElementById('cutting')
+var bulking = document.getElementById('bulking').addEventListener("click", bulking, false);
+var cutting = document.getElementById('cutting').addEventListener("click", cutting, false);
 var maintain = document.getElementById('maintain')
-var weight = document.getElementById('weight')
-var run = document.getElementById('run')
-run.addEventListener("click")
+var weight = document.getElementById('weight');
+var dropdown = document.getElementById('topdown');
 
-function bulking(){
-if (bulking == true){
-  cals = weight * 22;
-  document.getElementById('cals').innerHTML = 'Recommended cals' + cals;
-}
+
+function bulking() {
+  dropdown.innerHTML = "Bulking";
+  weight = weight.value;
+  cals = weight * 23.5;
+  document.getElementById('cals').innerHTML = 'Recommended cals ' + cals;
+  
+
 };
-let massPopChart = new Chart(worldChart,{
-      type:'doughnut',
-      data:{
-        labels:['carbs', 'fats', 'protien', 'sugars'],
-        datasets:[{
-        label:'worldwide population in billions',
-        data:[
-          7.07,
-          7.16,
-          7.25,
-          7.34
+
+let bulkingChart = new Chart(chart, {
+    type: 'doughnut',
+    data: {
+      labels: ['carbs', 'fats', 'protein', 'sugars'],
+      datasets: [{
+        label: 'worldwide population in billions',
+        data: [
+          3.5*weight.value,
+          .3*weight.value,
+          1*weight.value,
+          .3*weight.value,
         ],
-        backgroundColor:['#F4F1DE', '#51344D', '#3D405B'],
-        borderWidth:1,
-        borderColor:'#777',
-        hoverBorderWidth:3,
-        hoverBorderColor:'#000'
-        }]
-      },
-      options:{}
-});
+        backgroundColor: ['#F4F1DE', '#51344D', '#3D405B'],
+        borderWidth: 1,
+        borderColor: '#777',
+        hoverBorderWidth: 3,
+        hoverBorderColor: '#000'
+      }]
+    },
+    options: {}
+  });
